25a53ccac4be88a0ddaaf829deffb83b6472b4ca
Zulfadli Rizal <zulfadlirizal@gmail.com>
2024-02-20 14:05:22 +0700

    Tugas 1

---
+++ tugas1.go
@@ -0,0 +1,13 @@
+/*Hari 1 - Command Line Interface dan Git;
+Data Peserta Bootcamp Digital Skill - Golang Backend Development
+1. Nama: Zulfadli Rizal
+2. Email: zulfadlirizal@gmail.com
+3. Sistem Operasi yang digunakan: MacOS
+4. Akun Github: https://github.com/bprbangunarta
+5. Akun Telegram: +6282320099971
+6. Role Pekerjaan (BE/FE/DEVOPS/MAHASISWA) : BE
+7. Umur: 26 Tahun
+8. Pernah menggunakan Go? : Belum pernah
+9. Bahasa Pemrograman yg pernah dipelajari: PHP
+10.Pernah membuat REST API? : Pernah
+*/
