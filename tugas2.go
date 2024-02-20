beaded8394cf4d40eb13868a1711cb0473fe1e85
Zulfadli Rizal <zulfadlirizal@gmail.com>
2024-02-20 14:31:38 +0700

    Tugas 2

--- tugas2.go
+++ tugas2.go
@@ -5,9 +5,16 @@ import (
 )
 
 func main() {
-	var text = "Hello World"
-	fmt.Println(text)
+	// contoh 1
+	var message = `Nama saya "Zulfadli Rizal". Salam kenal . Mari belajar "Golang".`
 
-	text = "Hello World"
-	fmt.Println(text)
+	fmt.Println(message)
+
+	// contoh 2
+	var name = "Zulfadli Rizal"
+	var age = "26"
+	var sentence = `halo nama saya "` + name + `" dan berumur "` + age + `"`
+
+	fmt.Println()
+	fmt.Println(sentence)
 }
